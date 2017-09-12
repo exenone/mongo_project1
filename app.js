77d63ce6918d76b3adc0161345bd5f322f29d2d7
exenone <seeunuwan@gmail.com>
2017-09-13 00:14:37 +0200

    /initial commit

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express=require('express');
-const app=express();
+var express=require('express');
+var app=express();
 var MongoClient = require('mongodb').MongoClient;
 var db = "mongodb://localhost:27017/mydb";
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs'); 
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+
+app.use(require('express-method-override')('method_override_param_name'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/views')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+/*
+app.post('/user', urlencodedParser, function (req, res) {
+    res.send("Submitted user's name is: " + req.body.name);
+});
+*/
+
+
+app.post('/insert', function(req, res){
+  res.send("data inject is succesfull!!"+req.body.tree);  
+});
+
+
+
+
+
 
 
 const http = require('http');
@@ -11,51 +37,6 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(express.static('src/views'));
-
-//-----------------------------------------------------------------------------------------
-
-app.get('/', function(req,res){
-    res.render('fixture');
-})
- 
-app.post('/insert', function (req, res) {
-
-MongoClient.connect(db, function(err, db) {
-    if (err) throw err;
-    
-    db.collection('students', function(err, collection) {
-        doc = {
-              "tree" : req.body.tree,
-              "xxx" : req.body.xxx
-        };
-        collection.insert(doc, function() {
-            res.send('Inserted Successfully!');
-            db.close();
-        });
-    });
-});
-
-  
-});
-   
-
-  // --http://www.wesohaex.com/nodejs/insert-data-mongodb-using-nodejs.html-------------------------------------
-app.get('/',function(req,res){
-    res.send('welcome!');
-  
-  });
-  
-  app.get('/routing',function(req,res){
-    res.send('welcome!its routing');
-  
-  });
-
-  app.all('/secret', function (req, res, next) {
-    console.log('Accessing the secret section ...')
-    next() 
-  });
 
   app.listen(port, hostname, (err) => {
     console.log(`Server running at http://${hostname}:${port}/`);
